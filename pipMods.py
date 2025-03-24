bb07fa68235e28a8bf0622aa437a1b8eadfb8ffe
Marsyleeno <138357018+Marsyleeno@users.noreply.github.com>
2025-03-24 22:47:49 +0800

    Create pipMods.py

---
+++ pipMods.py
@@ -0,0 +1,17 @@
+import math
+import threading
+import numpy as np
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+from mpl_toolkits.mplot3d.art3d import Poly3DCollection
+from kivy.app import App
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.label import Label
+from kivy.uix.textinput import TextInput
+from kivy.uix.button import Button
+from kivy.uix.popup import Popup
+from kivy.uix.spinner import Spinner
+import pygame
+
+--- python packages ---
